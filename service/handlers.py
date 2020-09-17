041c2d233d36e959aeaaed8b0b9c4cf0e6eb4b21
gabriellsesam <gabriell.vig@sesam.io>
2020-09-17 16:09:09 +0200

    Don't update workorders, just create.

--- service/handlers.py
+++ service/handlers.py
@@ -8,9 +8,9 @@ def elwin_workorder(s: Session, url: str, entity: dict):
 
     def do_workorders_request(session: Session, method: str, entity: dict):
         resp = None
-        if method == 'update':
-            resp = session.put(f'{url}/ElWinAvtaler/api/workorders', json=entity)
-        elif method == 'get':
+        #if method == 'update':         # We won't update
+        #    resp = session.put(f'{url}/ElWinAvtaler/api/workorders', json=entity)
+        if method == 'get':
             resp = session.get(f'{url}/ElWinAvtaler/api/workorders?externalIds={entity["ExternalId"]}')
         elif method == 'create':
             resp = session.post(f'{url}/ElWinAvtaler/api/workorders', json=entity)
@@ -36,11 +36,13 @@ def elwin_workorder(s: Session, url: str, entity: dict):
         "Message": entity.get('Description') # response_entity.get('Description')
     }
     if entity.get('Id', None) is not None:
-        try:
-            do_workorders_request(s, 'update', entity)
-            message_entity['WorkorderId'] = entity.get('Id', 0)
-        except json.JSONDecodeError as e:
-            logger.debug(f'Could not UPDATE entity with id: "{entity.get("_id", None)}" because of error: "{e}"')
+        logger.warning('We wont update this entity.')
+        pass     #We wont update workorders.
+#        try:
+#            do_workorders_request(s, 'update', entity)
+#            message_entity['WorkorderId'] = entity.get('Id', 0)
+#        except json.JSONDecodeError as e:
+#            logger.debug(f'Could not UPDATE entity with id: "{entity.get("_id", None)}" because of error: "{e}"')
 
     else:  # Try to find entity based on externalId
         # If response is not JSON then we need to create the entity
@@ -63,22 +65,25 @@ def elwin_workorder(s: Session, url: str, entity: dict):
 
         # If we find the Id then we update, else we create.
         if response_entity.get('Id', None) is not None:
-            entity['Id'] = response_entity['Id']
-            try:
-                do_workorders_request(s, 'update', entity)
-                message_entity['WorkorderId'] = response_entity.get('Id', 0)
-            except json.JSONDecodeError as e:
-                logger.debug(f'Could not UPDATE entity with id: "{entity.get("_id", None)}" because of error: "{e}". Entity: {entity}')
+            logger.warning('We wont update this entity.')
+            pass             # We don't update.
+            # entity['Id'] = response_entity['Id']
+            # try:
+            #     do_workorders_request(s, 'update', entity)
+            #     message_entity['WorkorderId'] = response_entity.get('Id', 0)
+            # except json.JSONDecodeError as e:
+            #     logger.debug(f'Could not UPDATE entity with id: "{entity.get("_id", None)}" because of error: "{e}". Entity: {entity}')
         else:
             if 'Id' in entity:
                 del entity['Id']
             try:
                 created_entity = json.loads(do_workorders_request(s, 'create', entity))
                 message_entity['WorkorderId'] = created_entity.get('Id', 0)
+
+                if message_entity.get('WorkorderId', 0) != 0 or message_entity.get('Description') is not None:
+                    do_workordermessages_request(s, message_entity)
+                else:
+                    logger.error(
+                        f'Could not send workordermessage request because the description or workorderid is null: Entity: {message_entity}')
             except json.JSONDecodeError as e:
                 logger.debug(f'Could not CREATE entity with id: "{entity.get("_id", None)}" because of error: "{e}". Entity: {entity}')
-
-    if message_entity.get('WorkorderId', 0) != 0 or message_entity.get('Description') is not None:
-        do_workordermessages_request(s, message_entity)
-    else:
-        logger.error(f'Could not send workordermessage request because the description or workorderid is null: Entity: {message_entity}')
